37a39550804acff4daf4323a48f8925b4579389d
Philip <filipplitskevich99@gmail.com>
2024-01-10 12:30:25 +0300

    gh add mobile map

--- js/app.js
+++ js/app.js
@@ -7160,6 +7160,173 @@
         };
         const da = new DynamicAdapt("max");
         da.init();
+        initMap();
+        async function initMap() {
+            await ymaps3.ready;
+            const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+            const {YMapDefaultMarker} = await ymaps3.import("@yandex/ymaps3-markers@0.0.1");
+            const map = new YMap(document.getElementById("map"), {
+                location: {
+                    center: [ 38.333503, 45.013715 ],
+                    zoom: 16
+                }
+            });
+            map.addChild(new YMapDefaultSchemeLayer({
+                theme: "dark",
+                customization: [ {
+                    tags: {
+                        all: [ "water" ]
+                    },
+                    elements: "geometry",
+                    stylers: [ {
+                        color: "#60242B"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "water" ]
+                    },
+                    elements: "label.text.fill",
+                    stylers: [ {
+                        color: "#1F1222"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "water" ]
+                    },
+                    elements: "label.text.outline",
+                    stylers: [ {
+                        color: "#6A252C"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "landscape" ]
+                    },
+                    elements: "geometry",
+                    stylers: [ {
+                        color: "#6F2721"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "land" ]
+                    },
+                    elements: "geometry",
+                    stylers: [ {
+                        color: "#67221D"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "transit" ]
+                    },
+                    elements: "geometry",
+                    stylers: [ {
+                        color: "#6F222B"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "structure" ]
+                    },
+                    elements: "geometry",
+                    stylers: [ {
+                        color: "#762428"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "road" ]
+                    },
+                    elements: "geometry",
+                    stylers: [ {
+                        color: "#83282C"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "road" ]
+                    },
+                    elements: "geometry.fill",
+                    stylers: [ {
+                        color: "#83282C"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "road" ]
+                    },
+                    elements: "geometry.outline",
+                    stylers: [ {
+                        color: "#702326"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "admin" ]
+                    },
+                    elements: "geometry.fill",
+                    stylers: [ {
+                        color: "#83282C"
+                    } ]
+                }, {
+                    tags: {
+                        all: [ "admin" ]
+                    },
+                    elements: "geometry.outline",
+                    stylers: [ {
+                        color: "#6E2225"
+                    } ]
+                }, {
+                    tags: {
+                        any: [ "poi", "transit_location" ]
+                    },
+                    elements: "label.text.fill",
+                    stylers: [ {
+                        color: "#241727"
+                    } ]
+                }, {
+                    tags: {
+                        any: [ "poi", "transit_location" ]
+                    },
+                    elements: "label.text.outline",
+                    stylers: [ {
+                        color: "#84292D"
+                    } ]
+                }, {
+                    tags: {
+                        any: [ "shopping" ]
+                    },
+                    elements: "label.icon",
+                    stylers: [ {
+                        color: "#321B2A"
+                    } ]
+                }, {
+                    tags: {
+                        any: [ "food_and_drink" ]
+                    },
+                    elements: "label.icon",
+                    stylers: [ {
+                        color: "#851A11"
+                    } ]
+                }, {
+                    tags: {
+                        any: [ "admin", "road" ]
+                    },
+                    elements: "label.text.fill",
+                    stylers: [ {
+                        color: "#200A0B"
+                    } ]
+                }, {
+                    tags: {
+                        any: [ "admin", "road" ]
+                    },
+                    elements: "label.text.outline",
+                    stylers: [ {
+                        color: "#762428"
+                    } ]
+                } ]
+            }));
+            const defaultMarker = new YMapDefaultMarker({
+                coordinates: [ 38.333503, 45.013715 ],
+                color: "#AA464B"
+            });
+            map.addChild(new ymaps3.YMapDefaultFeaturesLayer({
+                zIndex: 1800
+            })).addChild(defaultMarker);
+        }
         document.addEventListener("watcherCallback", (function(e) {
             const entry = e.detail.entry;
             const targetElement = entry.target;
